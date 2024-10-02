cf5d443f748572c48b63b40c7f6aaf3d7e16c5f6
chikuwa-ah <chikuwah21@gmail.com>
2024-10-02 23:05:14 +0900

    first one step!!!!!!!!!

--- calculation.js
+++ calculation.js
@@ -1,9 +1,15 @@
-/** @type {HTMLBodyElement} */
 'use strict';
 
-const home = () => {
-    console.log('home');
+const title = () => {
+    const startButton = document.getElementById('title').children[1];
+    startButton.addEventListener('click', () => {
+        const audio = new Audio('./se/correct.mp3');
+        audio.play();
+    }, { once: true });
+};
 
-}
-
-home();
+window.addEventListener('DOMContentLoaded', () => {
+    const main_element = document.getElementById('main');
+    main_element.style.display = 'none';
+    title();
+});
