512e480ed3e159c2e16f434c66284789ed4ee2a8
chikuwa-ah <chikuwah21@gmail.com>
2024-10-03 00:02:46 +0900

    third function !!!

--- calculation.js
+++ calculation.js
@@ -1,15 +1,26 @@
 'use strict';
 
+const generateFormula = () => {
+    const mainElement = document.getElementById('main');
+    mainElement.style.display = 'flex';
+}
+
 const title = () => {
-    const startButton = document.getElementById('title').children[1];
-    startButton.addEventListener('click', () => {
-        const audio = new Audio('./se/correct.mp3');
-        audio.play();
+    const titleHTMLElement = document.getElementById('title');
+    const titleChildren = titleHTMLElement.children;
+    titleChildren[1].children[0].addEventListener('click', () => {
+        titleChildren[0].textContent = 'START!';
+        titleChildren[1].style.display = 'none';
+
+        setTimeout(() => {
+            titleHTMLElement.style.display = 'none';
+            generateFormula();
+        }, 1500);
     }, { once: true });
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-    const main_element = document.getElementById('main');
-    main_element.style.display = 'none';
+    const mainElement = document.getElementById('main');
+    mainElement.style.display = 'none';
     title();
 });
